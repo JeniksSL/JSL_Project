535fe1cf11c03529dadcb67d94dc24354880ef2f
JeniksSL <iba.slabko1.22@gmail.com>
2023-11-24 14:33:50 +0300

    toolbar

--- app/src/main/java/com/iba/jslproject/activity/AuthActivity.kt
+++ app/src/main/java/com/iba/jslproject/activity/AuthActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.iba.jslproject.R
 import com.iba.jslproject.databinding.ActivityAuthBinding
 import timber.log.Timber
@@ -34,9 +36,29 @@ class AuthActivity : AppCompatActivity() {
                 Timber.d("Error in user creation: ${it.exception.toString()}")
                 if (it.exception is FirebaseAuthInvalidCredentialsException) {
                     Toast.makeText(applicationContext, (it.exception as FirebaseAuthInvalidCredentialsException).message.toString(), Toast.LENGTH_SHORT).show()
+                } else if (it.exception is FirebaseAuthUserCollisionException) {
+                    tryLogin(email, password)
+                } else {
+                    Toast.makeText(applicationContext,
+                        getString(R.string.general_error_message), Toast.LENGTH_SHORT).show()
                 }
             }
 
+        }
+    }
+
+    fun tryLogin(email:String, password:String) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            if(it.isSuccessful) {
+                Timber.d("User signed in successfully: ${FirebaseAuth.getInstance().currentUser}")
+                successfulAuth()
+            } else if (it.exception is FirebaseException) {
+                Timber.d("Error in signing in: ${it.exception.toString()}")
+                Toast.makeText(applicationContext, (it.exception as FirebaseException).message.toString(), Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext,
+                    getString(R.string.general_error_message), Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
